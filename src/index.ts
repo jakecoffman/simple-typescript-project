04e8c798c506ce0387c334b71a135e196313d2f3
Jake Coffman <jake@jakecoffman.com>
2024-08-30 14:00:34 -0500

    breakable

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import _ from 'lodash';
 
-console.log("HELLO WORLD");
+function getFirstElement<T>(list: _.List<T>): T | undefined {
+    return _.first(list); // _.first is a lodash function that gets the first element of an array or list
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const firstNumber = getFirstElement(numbers);
+console.log(firstNumber); // Output: 1
